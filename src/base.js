3802833b0a2dd0a76dab5b9b1582fa728562bfdb
Ankit Tiwari <ankitt255@gmail.com>
2017-12-20 19:55:39 +0530

    Add firebase for persistent fishes state

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyD5IBCZFPzCid_ZI1rIsvrcoIkcyqCWnS4",
+  authDomain: "catch-of-the-day-91c04.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-91c04.firebaseio.com"
+});
+
+export default base;
