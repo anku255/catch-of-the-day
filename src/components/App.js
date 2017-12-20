124382b7eb96094fef935ec95c3de195d4cdba8e
Ankit Tiwari <ankitt255@gmail.com>
2017-12-20 16:18:06 +0530

    Add a fn addToOrder to update order state

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
 
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
 
     // getinitialState
     this.state = {
@@ -33,6 +34,15 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   }
 
+  addToOrder(key) {
+    // make a copy of current state
+    const order = { ...this.state.order };
+    // update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    // update our state
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -42,7 +52,14 @@ class App extends React.Component {
             {
               Object
                 .keys(this.state.fishes)
-                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+                .map(key =>
+                  <Fish
+                    key={key}
+                    index={key}
+                    details={this.state.fishes[key]}
+                    addToOrder={this.addToOrder}
+                  />
+                )
             }
           </ul>
         </div>
