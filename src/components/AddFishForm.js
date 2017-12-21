8f353b2ff1a9898e74f3f84047164b42d4ed122c
Ankit Tiwari <ankitt255@gmail.com>
2017-12-21 12:38:30 +0530

    Set propTypes to validate props

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 class AddFishForm extends React.Component {
-  
+
   createFish(event) {
     event.preventDefault();
     const fish = {
@@ -32,6 +32,10 @@ class AddFishForm extends React.Component {
       </form>
     );
   }
+}
+
+AddFishForm.propTypes = {
+  addFish: React.PropTypes.func.isRequired
 }
 
 export default AddFishForm;
